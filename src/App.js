7a29a8b8b810c050eae07253b279baece885a1c8
Aman kumar <amankumar12091999@gmail.com>
2023-06-09 18:42:08 +0530

    Feat: added Navbar and Sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./Login";
+import { Home } from "./Home";
+import { Layout } from "./component/Layout.jsx";
 import { Signup } from "./Signup";
 import "./App.css";
 
@@ -7,6 +9,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/home" element={<Home />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
